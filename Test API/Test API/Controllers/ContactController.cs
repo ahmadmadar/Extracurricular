e99eb1895d0c041bbd81daca05dfd8cd82ae2a57
ahmadmadar <35266736+ahmadmadar@users.noreply.github.com>
2020-04-07 18:46:01 -0600

    Added Test API project (Search/Add button functional)

---
+++ Test API/Test API/Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Test_API.Models;
+
+namespace Test_API.Controllers
+{
+    public class ContactController : ApiController
+    {
+        Contacts[] contacts = new Contacts[]
+        {
+            new Contacts() {ContactID = 0, FirstName = "Kyle", LastName = "Lowry"},
+            new Contacts() {ContactID = 1, FirstName = "Pascal", LastName = "Siakam"},
+            new Contacts() {ContactID = 2, FirstName = "Fred", LastName = "VanVleet"}
+
+        };
+
+        // GET: api/Contact
+        public IEnumerable<Contacts> Get()
+        {
+            return contacts;
+        }
+
+        // GET: api/Contact/5
+        public IHttpActionResult Get(int id)
+        {
+            Contacts contact = contacts.FirstOrDefault(c => c.ContactID == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // POST: api/Contact
+        public IEnumerable<Contacts> Post([FromBody]Contacts newContact)
+        {
+            List<Contacts> contactList = contacts.ToList();
+            newContact.ContactID = contactList.Count();
+            contactList.Add(newContact);
+            contacts = contactList.ToArray();
+
+            return contacts;
+        }
+
+        // PUT: api/Contact/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Contact/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Test API/Test API/Models/Contacts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_API.Models
+{
+    public class Contacts
+    {
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contacts()
+        {
+            ContactID = -1;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
